380a621c9492fea665f9ca24604b703443b16b42
Siumauricio <siumauricio@unitec.edu>
2022-08-04 20:35:49 -0600

    fix: removed issue not detecting web in window object

--- utils/web3.ts
+++ utils/web3.ts
@@ -1,6 +1,9 @@
 import Web3 from 'web3';
 let web3: Web3;
-if (typeof window !== undefined && typeof window.web3 !== undefined) {
+if (
+  typeof window !== 'undefined' &&
+  typeof window.web3 !== 'undefined'
+) {
   web3 = new Web3(window.web3.currentProvider);
 } else {
   const provider = new Web3.providers.HttpProvider(
